6cd55fe159b724cbade55b01bdcf1120835d55e7
aathya <sunil.a@agrya.in>
2022-12-29 11:25:52 +0000

    data added

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# ideal-waffle
-ruby hash parser
+## Hash Manipulator
+
+```
+[
+  {
+    "name": "sunil",
+    "subject": "Maths",
+    "mark": 100
+  },
+  {
+    "name": "sunil",
+    "subject": "Science",
+    "mark": 150
+  },
+  {
+    "name": "aathy",
+    "subject": "CS",
+    "mark": 150
+  }
+]
+```
+
+The above array should be manipulated.
+
+In the above, name is the root key, using that we have to group by and sum.
+
+It should return like below
+
+```
+HashManipulator.new(data, rows: ['name'], columns: ['subject'], aggregates: ['mark'], include_aggregate_as_key: false)
+
+[
+  {
+    "name": "Sunil",
+    "Maths": 100,
+    "Science": 150
+  },
+  {
+    "name": "aathy",
+    "Science": 150
+  }
+]
+```
+
+We can give **N number** of _columns, rows & aggregates_
+
+If include_aggregate_as_key is `true` the result will be like
+
+```
+[
+  {
+    "name": "Sunil",
+    "Maths": {
+      "mark": 100
+    },
+    "Science": {
+      "mark": 150
+    }
+  },
+  {
+    "name": "aathy",
+    "Science": {
+      "mark": 150
+    }
+  }
+]
+```
---
+++ data.rb
@@ -0,0 +1,233 @@
+def data_one
+  data     = [
+    {
+      client:   'ABC',
+      location: 'Banglore',
+      amount:   100
+    },
+    {
+      client:   'ABC',
+      location: 'Banglore',
+      amount:   300
+    },
+    {
+      client:   'ABC',
+      location: 'Chennai',
+      amount:   200
+    },
+    {
+      client:   'BCD',
+      location: 'Banglore',
+      amount:   40
+    },
+    {
+      client:   'BCD',
+      location: 'Chennai',
+      amount:   75
+    },
+    {
+      client:   'XYZ',
+      location: 'Banglore',
+      amount:   80
+    },
+    {
+      client:   'XYZ',
+      location: 'Chennai',
+      amount:   300
+    },
+    {
+      client:   'XYZ',
+      location: 'Rome',
+      amount:   750
+    },
+  ].map(&:with_indifferent_access)
+  response = [
+    {
+      "client":   'ABC',
+      "Banglore": { "amount": 400 },
+      "Chennai":  { "amount": 200 },
+    },
+    {
+      "client":   'BCD',
+      "Banglore": { "amount": 40 },
+      "Chennai":  { "amount": 75 },
+    },
+    {
+      "client":   'XYZ',
+      "Banglore": { "amount": 80 },
+      "Chennai":  { "amount": 300 },
+      "Rome":     { "amount": 750 }
+    }
+  ]
+  [data, response]
+end
+
+def data_two
+  data     = [
+    {
+      client:   'ABC',
+      location: 'Banglore',
+      month:    'May',
+      amount:   100
+    },
+    {
+      client:   'ABC',
+      location: 'Banglore',
+      month:    'Jun',
+      amount:   250
+    },
+    {
+      client:   'ABC',
+      location: 'Chennai',
+      month:    'May',
+      amount:   120
+    },
+    {
+      client:   'ABC',
+      location: 'Chennai',
+      month:    'Jun',
+      amount:   220
+    },
+    {
+      client:   'BCD',
+      location: 'Banglore',
+      month:    'May',
+      amount:   110
+    },
+    {
+      client:   'BCD',
+      location: 'Banglore',
+      month:    'Jun',
+      amount:   140
+    },
+    {
+      client:   'BCD',
+      location: 'Chennai',
+      month:    'May',
+      amount:   130
+    },
+    {
+      client:   'BCD',
+      location: 'Chennai',
+      month:    'Jun',
+      amount:   130
+    }
+  ].map(&:with_indifferent_access)
+  response = [
+    {
+      client:   'ABC',
+      Banglore: {
+        "May": 100,
+        "Jun": 250
+      },
+      Chennai:  {
+        "May": 120,
+        "Jun": 220
+      }
+    },
+    {
+      client:   'BCD',
+      Banglore: {
+        "May": 110,
+        "Jun": 140
+      },
+      Chennai:  {
+        "May": 130,
+        "Jun": 130
+      }
+    }
+  ]
+  [data, response]
+end
+
+def data_three
+  data     = [
+    {
+      client:   'ABC',
+      location: 'Banglore',
+      month:    'May',
+      amount:   100,
+      amount2:  34
+    },
+    {
+      client:   'ABC',
+      location: 'Banglore',
+      month:    'May',
+      amount:   140,
+      amount2:  38
+    },
+    {
+      client:   'ABC',
+      location: 'Banglore',
+      month:    'Jun',
+      amount:   250,
+      amount2:  100
+    },
+    {
+      client:   'ABC',
+      location: 'Chennai',
+      month:    'May',
+      amount:   120,
+      amount2:  150
+    },
+    {
+      client:   'ABC',
+      location: 'Chennai',
+      month:    'Jun',
+      amount:   220
+    },
+    {
+      client:   'BCD',
+      location: 'Banglore',
+      month:    'May',
+      amount:   110
+    },
+    {
+      client:   'BCD',
+      location: 'Banglore',
+      month:    'Jun',
+      amount:   140
+    },
+    {
+      client:   'BCD',
+      location: 'Chennai',
+      month:    'May',
+      amount:   130
+    },
+    {
+      client:   'BCD',
+      location: 'Chennai',
+      month:    'Jun',
+      amount:   130
+    }
+  ].map(&:with_indifferent_access)
+  response = [
+    {
+      "client"   => "ABC",
+      "Banglore" =>
+        {
+          "May" => { "amount" => 240, "amount2" => 72 },
+          "Jun" => { "amount" => 250, "amount2" => 100 }
+        },
+      "Chennai"  =>
+        {
+          "May" => { "amount" => 120, "amount2" => 150 },
+          "Jun" => { "amount" => 220, "amount2" => nil }
+        }
+    },
+    {
+      "client"   => "BCD",
+      "Banglore" =>
+        {
+          "May" => { "amount" => 110, "amount2" => nil },
+          "Jun" => { "amount" => 140, "amount2" => nil }
+        },
+      "Chennai"  =>
+        {
+          "May" => { "amount" => 130, "amount2" => nil },
+          "Jun" => { "amount" => 130, "amount2" => nil }
+        }
+    }
+  ]
+  [data, response]
+end
